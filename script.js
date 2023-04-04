a18cd03e099767cf80f0a3762a68e9e14e05d38b
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-04 11:09:05 +0700

    Add classes template with two constructors then in the code block we refer the parameters to this class with this name equals name. Then to create a new class we can store it in a variable then use the new keyword followd by the class name then the property values in parentheses.

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ console.log(book100);
 
 class Book {
 constructor(author, title) {
+this.author = author;
+this.title = title;
+}
+}
+const book1 = new Book("Mathew Walker", "Why We Sleep");
+console.log(book1);
 
-}
-}
 class Animal {
     constructor(name, species) {
-        
+this.name = name;
+this.species = species;
     }
 }
+const animal1 = new Animal("Scooby Doo", "Doggo");
+console.log(animal1)
+
+
+
