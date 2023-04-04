dc0977fde719589ff64b722a7a1034660d865f58
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-04 10:42:25 +0700

    Add new file for classes code examples. Create object to compare with structure and syntax of a class and the constructor with property values in parentheses.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+const book100 = {
+    author: "William Shakespeare",
+    title: "The Davinci Code",
+}
+console.log(book100);
+
+class Book {
+constructor(author, title) {
+
+}
+}
+class Animal {
+    constructor(name, species) {
+        
+    }
+}
