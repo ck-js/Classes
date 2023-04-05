0af400a92738145e47d6f8f3839eb3d84dcb3e84
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-05 11:53:14 +0700

    Add inheritance examples of sub classes inheriting methods from super classes.

--- inheritance.js
+++ inheritance.js
@@ -13,8 +13,18 @@ class Apliances {
     constructor(brand) {
         this.brand = brand;
     }
+    turnOn() {
+        console.log(this.brand + " is turning on");
+    }
 }
 class Dishwasher extends Apliances {   
 };
 const dishwasher1 = new Dishwasher("Mitsubishi");
 console.log(dishwasher1);
+
+// advance inheritance 
+console.log(dishwasher1.brand);
+dishwasher1.turnOn();
+class WashingMachine extends Apliances {};
+const washingMachine1 = new WashingMachine("Bosch"); 
+washingMachine1.turnOn(); 
