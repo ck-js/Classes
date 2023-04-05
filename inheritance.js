5701a4a1d2a46922943412d2bcdc172ada93ffea
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-05 14:53:15 +0700

    Add superclass with method that passes a value from the constructor. Create subclass with the extends keyword and remember to define the constructor parameters and or method parameters when creating instances of sub class objects.

--- inheritance.js
+++ inheritance.js
@@ -28,3 +28,34 @@ dishwasher1.turnOn();
 class WashingMachine extends Apliances {};
 const washingMachine1 = new WashingMachine("Bosch"); 
 washingMachine1.turnOn(); 
+
+// adding methods to sub classes 
+class Book {
+    constructor(title) {
+        this.title = title;
+        this.page = 1;
+    }
+    setBookmark(page) {
+        this.page = page;
+    }
+}
+class Cookbook extends Book {
+};
+const cookBook1 = new Cookbook("Why We Sleep");
+cookBook1.setBookmark(11);
+console.log(cookBook1)
+
+class Employee {
+    constructor(company) {
+        this.company = company;
+    };
+    clockIn() {
+        console.log("Clocking in for " + this.company);
+    }
+} 
+class Lawyer extends Employee {};
+const lawyer1 = new Lawyer("Anantara");
+console.log(lawyer1);
+lawyer1.clockIn();
+
+
