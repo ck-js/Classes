b100ce2dda9b04c33d90984b4eeb66d4745279b0
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-05 19:53:18 +0700

    Add a superclass and sub class with the same parameters but different method names to show that classes with same parameters wont override each other as they need to be the same method name.

--- overrideMethods.js
+++ overrideMethods.js
@@ -6,10 +6,17 @@ class Human {
     receiveDamage() {
         this.health = this.health - 10;
     }
+    attack() {
+        console.log("Swing " + this.weapon);
+    }
 }
 class Wizard extends Human {
     receiveDamage() {
         this.health -= 5;
+    }
+    attack() {
+        super.attack();
+        console.log("Zim Zoom");
     }
 } 
 
@@ -24,3 +31,25 @@ human1.receiveDamage();
 wizard1.receiveDamage();
 console.log(human1.health);
 console.log(wizard1.health)
+
+human1.attack();
+wizard1.attack();
+
+class Mammal {
+    eat(food) {
+        console.log(food + " nom nom");
+    }
+}
+class Rabbit extends Mammal {
+    eatVeggies(food) {
+        if (food === "Lettuce") {
+            console.log(food + "nom nom");
+        }
+    }
+}
+const mammal1 = new Mammal;
+const rabbit1 = new Rabbit;
+
+mammal1.eat("Ant");
+rabbit1.eatVeggies("Chicken");
+
